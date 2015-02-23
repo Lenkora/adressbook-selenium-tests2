44cb8d611ae6a7bcff0724530a1b47644d37c8dc
Lena <nikolskaya_elena@mail.ru>
2015-02-23 17:12:54 +0300

    java22

---
+++ src/com/example/tests/AlsoGroupCreationTests.java
@@ -0,0 +1,27 @@
+package com.example.tests;
+import org.testng.annotations.Test;
+public class AlsoGroupCreationTests extends TestBase  {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	  openMainPage();
+    openGroupsPage();
+    initGroupCreation();
+    GroupData group = new GroupData();
+    group.name=" group name 1";
+    		group.header=" group header 1";
+    				group.footer=" group footer 1";
+	fillGroupForm(group);
+    siubmitGroupCreation();
+    returnToGroupPage();
+  }
+  
+  @Test
+  public void testEmptyGroupCreation() throws Exception {
+	  openMainPage();
+    openGroupsPage();
+    initGroupCreation();
+    fillGroupForm(new GroupData("", "", ""));
+    siubmitGroupCreation();
+    returnToGroupPage();
+  }
+}
